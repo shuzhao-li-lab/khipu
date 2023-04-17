855d7535f905481743000c45ac7b036a25f39071
Yuanye Chi <chiyuanye@chiyuanye.com>
2023-04-17 11:54:51 -0400

    set html theme as readthedoc default

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,5 +49,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
