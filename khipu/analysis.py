76a9c753cefcb2021b976b58ee1f8ba8913f43e0
Yuanye Chi <chiyuanye@chiyuanye.com>
2024-04-21 23:51:58 -0400

    adjusted for server

--- khipu/analysis.py
+++ khipu/analysis.py
@@ -4,7 +4,8 @@ import numpy as np
 import json
 import isocor
 import pandas as pd
-import requests as re
+import requests as req
+import re
 import os
 from decimal import Decimal
 from mass2chem.formula import parse_chemformula_dict, dict_to_hill_formula
@@ -26,7 +27,7 @@ TRACER_ABUNDANCE_VECTORS = {
     "15N": [0, 100],
 }
 
-def get_all_isotope_data():
+def get_all_isotope_data(nist_path='.'):
     def _stripColNames(df):
         df.rename(columns=lambda x: x.strip())
 
@@ -43,8 +44,8 @@ def get_all_isotope_data():
             d[e] = dg
         return d
     
-    if not os.path.exists("./all_isotopes_NIST.csv"):
-        r = re.get("https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl?ele=&all=all&ascii=ascii2&isotype=all")
+    if not os.path.exists(nist_path):
+        r = req.get("https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl?ele=&all=all&ascii=ascii2&isotype=all")
         isotopes = []
         ele, mass, try_nap, nap = None, None, False, None
         for l in r.text.split("\n"):
@@ -71,31 +72,28 @@ def get_all_isotope_data():
                     })
                 ele, mass, try_nap, nap = None, None, False, None
         df = pd.DataFrame(isotopes)
-        df.to_csv("all_isotopes_NIST.csv", sep=",")
-    with open(str("all_isotopes_NIST.csv"), 'r', encoding='utf-8') as fp:
+        df.to_csv(nist_path, sep=",")
+    with open(str(nist_path), 'r', encoding='utf-8') as fp:
         dfIsotopes = pd.read_csv(fp, converters={'mass': Decimal, 'abundance': np.float64})
     _stripColNames(dfIsotopes)
     _stripCol(dfIsotopes, ['element', ])
     dictIsotopes = _makeIsotopesDict(dfIsotopes)
     return dictIsotopes
 
-ISOTOPE_DATA_COMPREHENSIVE = get_all_isotope_data()
+# ISOTOPE_DATA_COMPREHENSIVE = get_all_isotope_data()
 
 def detect_labelling(khipu_list, unlabeled_samples, labeled_samples, result_name, labeling_threshold=10, skip_isos=None):
     skip_isos = {"M0"} if skip_isos is None else skip_isos
-    L = []
     for khipu in khipu_list:
-        new_khipu = detect_labelling_khipu(khipu, unlabeled_samples, labeled_samples, result_name, labeling_threshold, skip_isos)
-        L.append(new_khipu)
-    return L
+        detect_labelling_khipu(khipu, unlabeled_samples, labeled_samples, result_name, labeling_threshold, skip_isos)
+    return khipu_list
 
 def detect_labelling_khipu(khipu, unlabeled_samples, labeled_samples, result_name, labeling_threshold=10, skip_isos=["M0"]):
     skip_isos = {"M0"} if skip_isos is None else skip_isos
     good_isotopologues = 0
     bad_isotopologues = 0
     for peak in khipu["MS1_pseudo_Spectra"]:
-        ion_relation = peak["ion_relation"]
-        isotope, _ = ion_relation.split(",")
+        isotope = peak["isotope"]
         if isotope not in skip_isos:
             unlabeled_avg = np.mean([peak[us] for us in unlabeled_samples])
             labeled_avg = np.mean([peak[s] for s in labeled_samples])
@@ -109,7 +107,6 @@ def detect_labelling_khipu(khipu, unlabeled_samples, labeled_samples, result_nam
         "good_labelling": good_isotopologues,
         "bad_labelling": bad_isotopologues
     }
-    return khipu
 
 def correct_natural_abundance(khipu_list, 
                               unlabeled_samples, 
@@ -119,7 +116,7 @@ def correct_natural_abundance(khipu_list,
                               resolution,
                               mz_of_resolution,
                               resolution_formula_code,
-                              ):
+                              nist_path):
     L = []
     for khipu in khipu_list:
         new_khipu = correct_natural_abundance_khipu(khipu, 
@@ -130,7 +127,8 @@ def correct_natural_abundance(khipu_list,
                                                     resolution,
                                                     mz_of_resolution,
                                                     resolution_formula_code,
-                                                    unique_only=False)
+                                                    nist_path,
+                                                    unique_only=False,)
         L.append(new_khipu)
     return L
 
@@ -141,13 +139,14 @@ def __build_isocor_corrector(formula,
                              resolution, 
                              mz_of_resolution,
                              resolution_formula_code,
-                             charge):
+                             charge,
+                             nist_path):
      return isocor.mscorrectors.MetaboliteCorrectorFactory(
         formula=formula,
         tracer=tracer,
         label=formula,
         inchi=None,
-        data_isotopes=ISOTOPE_DATA_COMPREHENSIVE,
+        data_isotopes=get_all_isotope_data(nist_path),
         derivative_formula=None,
         tracer_purity=[1 - tracer_purity, tracer_purity] if tracer_purity else None,
         correct_NA_tracer=True,
@@ -165,7 +164,8 @@ def correct_natural_abundance_khipu(khipu,
                                     resolution,
                                     mz_of_resolution,
                                     resolution_formula_code,
-                                    unique_only=True):
+                                    nist_path,
+                                    unique_only=True,):
     if "isocor_results" not in khipu:
         khipu["isocor_results"] = {}
 
@@ -184,6 +184,9 @@ def correct_natural_abundance_khipu(khipu,
         peak_lookup = {x['id']: x for x in khipu["MS1_pseudo_Spectra"]}
         for peak in khipu["MS1_pseudo_Spectra"]:
             adduct = peak["modification"]
+            # "M+H+, 3x charged". now it seems in one khipu all features have the same charge. Will change later
+            if 'charged' in adduct:
+                adduct = adduct.split(',')[0]
             if adduct not in abundance_vectors:
                 abundance_vectors[adduct] = {}
             isotope = peak["isotope"]
@@ -198,6 +201,12 @@ def correct_natural_abundance_khipu(khipu,
             for adduct, peaks_for_adduct in abundance_vectors.items():
                 khipu['isocor_results'][formula + "_" + adduct] = {}
                 formula_dict = parse_chemformula_dict(formula)
+
+                # the formula should have the element of the tracer
+                tracer_ele = re.findall(r'[A-Z]+', tracer)
+                if not tracer_ele[0] in formula_dict.keys():
+                    continue
+
                 for ele, count in ADDUCT_TO_FORMULA_DELTAS[adduct][2].items():
                     formula_dict[ele] = formula_dict.get(ele, 0) + count
                 adduct_corrected_formula = dict_to_hill_formula(formula_dict)
@@ -207,7 +216,8 @@ def correct_natural_abundance_khipu(khipu,
                                                     resolution,
                                                     mz_of_resolution,
                                                     resolution_formula_code,
-                                                    charge)
+                                                    charge,
+                                                    nist_path)
                 max_ele = parse_chemformula_dict(adduct_corrected_formula).get(TRACER_ELEMENT_MAP[tracer], 0)
                 if max_ele:
                     for ls in labeled_samples:
@@ -227,11 +237,12 @@ def correct_natural_abundance_khipu(khipu,
                                     iso += charge_string
                                 if iso not in khipu["isocor_results"][formula + "_" + adduct]:
                                     if f_id is not None:
-                                        khipu["isocor_results"][formula + "_" + adduct][iso] = peak_lookup[f_id]
+                                        khipu["isocor_results"][formula + "_" + adduct][iso] = dict(peak_lookup[f_id]) # deep copy, otherwise original peak will be changed
                                         for sample in labeled_samples:
                                             khipu["isocor_results"][formula + "_" + adduct][iso][sample] = 0
                                         for sample in unlabeled_samples:
-                                            del khipu["isocor_results"][formula + "_" + adduct][iso][sample]
+                                            if sample in khipu["isocor_results"][formula + "_" + adduct][iso].keys():
+                                                del khipu["isocor_results"][formula + "_" + adduct][iso][sample]
                                     else:
                                         khipu["isocor_results"][formula + "_" + adduct][iso] = {
                                             "apex": None,
@@ -257,6 +268,7 @@ def correct_natural_abundance_khipu(khipu,
                 khipu["isocor_results"][formula + "_" + adduct] = list(khipu["isocor_results"][formula + "_" + adduct].values())
     return khipu
      
+     
 def measure_overlap_with_GSMM(khipu_list, GEM, mz_tol=5):
     GEM_data = json.load(open(GEM))
     GEM_mz_tree = IntervalTree()
@@ -266,5 +278,5 @@ def measure_overlap_with_GSMM(khipu_list, GEM, mz_tol=5):
             mass_err = mass / 1e6 * mz_tol
             GEM_mz_tree.addi(mass - mass_err, mass + mass_err, cpd['id'])
     for khipu in khipu_list:
-        khipu["in_GEM"] = bool(GEM_mz_tree.at("neutral_formula_mass"))
+        khipu["in_GEM"] = bool(GEM_mz_tree.at(khipu["neutral_formula_mass"]))
     return khipu_list
